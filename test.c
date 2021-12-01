a1962f71b492ff2e10253e2b4532e7e7ea2e775a
LukyHena <39165888+LukyHena@users.noreply.github.com>
2021-12-01 13:45:55 +0100

    Update test.c

--- test.c
+++ test.c
@@ -1,36 +1,43 @@
+#include "ransomlib.h"
+#include <dirent.h> // for socket
+#include <sys/socket.h>
+#include <unistd.h> 
+#include <arpa/inet.h>
+#include <openssl/rand.h>
+
 // int generate_key(unsigned char *key, int sizeKey, unsigned char *iv, int sizeIv,char *pKey, char *pIv);
-int generate_key()
+// int generate_key()
+int generate_key(unsigned char *key, int sizeKey, unsigned char *iv, int sizeIv)
 {
-    // Key to use for encrpytion and decryption 
-    unsigned char key[AES_256_KEY_SIZE];
-    // Initialization Vector 
-    unsigned char iv[AES_BLOCK_SIZE];
-	
-	int sizeKey = AES_256_KEY_SIZE; 
-	int sizeIv = AES_BLOCK_SIZE;
-
 	// Generate cryptographically strong pseudo-random bytes for key and IV 
-    if (!RAND_bytes(key, sizeof(key)) || !RAND_bytes(iv, sizeof(iv))) 
-    {
-        // OpenSSL reports a failure, act accordingly 
-        fprintf(stderr, "ERROR: RAND_bytes error: %s\n", strerror(errno));
-        return errno;
-    }
+	if (!RAND_bytes(key, sizeKey) || !RAND_bytes(iv, sizeIv)) 
+	{
+		// OpenSSL reports a failure, act accordingly 
+		handleErrors();
+	}
 
-	for(int cpt=0; cpt<sizeKey; cpt++)
-	{
-			printf("%c", key[cpt]);
-	}
-	for(int cpt=0; cpt<sizeIv; cpt++)
-	{
-			printf("%c", iv[cpt]);
-	}
+	// for(int cpt=0; cpt<sizeKey; cpt++)
+	// {
+			// printf("%d", key[cpt]);
+	// }
+	printf("%d",key);
+	
+	// for(int cpt=0; cpt<sizeIv; cpt++)
+	// {
+			// printf("%ld", iv[cpt]);
+	// }
 	OPENSSL_cleanse(key,sizeof(key));	
 	OPENSSL_cleanse(iv,sizeof(iv));
 }
 
-
 int main ()
 {
-	generate_key();
+	// Key to use for encrpytion and decryption 
+	unsigned char key[AES_256_KEY_SIZE+1];
+	// Initialization Vector 
+	unsigned char iv[AES_BLOCK_SIZE+1];
+
+	int sizeKey = AES_256_KEY_SIZE; 
+	int sizeIv = AES_BLOCK_SIZE;
+	generate_key(key, sizeKey, iv, sizeIv);
 }
